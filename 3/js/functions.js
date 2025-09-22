d593e9a8f5ad95793e821c65359dd8527642d6bd
Keks <keks@htmlacademy.ru>
2025-09-22 10:24:41 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,21 @@
+const areStringBigger = function(string, targetLen){
+  return !(length(string) >= targetLen);
+};
+// Cтрока короче 20 символов
+areStringBigger('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+areStringBigger('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+areStringBigger('проверяемая строка', 10); // false
+
+const areStringPolyndrome = function(string){
+  return string.replaceAll(' ', '').reverse().toLowerCase() === string.replaceAll(' ', '').toLowerCase();
+};
+// Строка является палиндромом
+areStringPolyndrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+areStringPolyndrome('ДовОд'); // true
+// Это не палиндром
+areStringPolyndrome('Кекс');  // false
+// Это палиндром
+areStringPolyndrome('Лёша на полке клопа нашёл '); // true
