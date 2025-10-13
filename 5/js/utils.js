141fa08637502c6aa79f4a3415be396ad0a714a2
Keks <keks@htmlacademy.ru>
2025-10-13 17:43:43 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/utils.js
@@ -0,0 +1,30 @@
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export {createIdGenerator, getRandomInteger, createRandomIdFromRangeGenerator};
