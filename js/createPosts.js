3e11dab79b8c6d1a5d808353e92499b320f92c95
mem0sa <maksim.r.2007@mail.ru>
2025-12-18 01:41:36 +0500

    Реализация сортировки постов

--- js/createPosts.js
+++ js/createPosts.js
@@ -1,8 +1,18 @@
+import { debounce } from './utils.js';
+import { initiateBigPicture } from './bigPicture.js';
+
 const postsContainer = document.querySelector('.pictures');
-postsContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
-const fragment = document.createDocumentFragment();
+const filtersContainer = document.querySelector('.img-filters');
+let originalPosts = [];
+let currentPosts = [];
+
+const clearPosts = () => {
+  postsContainer.querySelectorAll('.picture').forEach((el) => el.remove());
+};
 
 const createPosts = function(posts){
+  postsContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
+  const fragment = document.createDocumentFragment();
   posts.forEach(({ comments, description, likes, url}, index) => {
     const pictureElement = document.createElement('a');
     pictureElement.dataset.index = index;
@@ -33,4 +43,62 @@ const createPosts = function(posts){
   postsContainer.appendChild(fragment);
 };
 
-export {createPosts};
+const renderPosts = (posts) => {
+  currentPosts = posts;
+  clearPosts();
+  createPosts(posts);
+};
+
+const debouncedRenderPosts = debounce(renderPosts, 500);
+
+const filterDefault = (posts) => posts.slice();
+
+const filterDiscussed = (posts) =>
+  posts
+    .slice()
+    .sort((a, b) => b.comments.length - a.comments.length);
+
+const getRandomPosts = (posts, count = 10) => {
+  const shuffled = posts.slice().sort(() => Math.random() - 0.5);
+  return shuffled.slice(0, count);
+};
+
+const setActiveFilter = (activeButton) => {
+  const buttons = filtersContainer.querySelectorAll('.img-filters__button');
+
+  buttons.forEach((button) => {
+    button.classList.toggle(
+      'img-filters__button--active',
+      button === activeButton
+    );
+  });
+};
+
+const onPostsLoaded = (posts) => {
+  originalPosts = posts.slice();
+  currentPosts = posts.slice();
+  filtersContainer.classList.remove('img-filters--inactive');
+  createPosts(originalPosts);
+};
+
+document.querySelector('#filter-default')
+  .addEventListener('click', (evt) => {
+    setActiveFilter(evt.target);
+    debouncedRenderPosts(filterDefault(originalPosts));
+  });
+
+document.querySelector('#filter-random')
+  .addEventListener('click', (evt) => {
+    setActiveFilter(evt.target);
+    debouncedRenderPosts(getRandomPosts(originalPosts));
+  });
+
+document.querySelector('#filter-discussed')
+  .addEventListener('click', (evt) => {
+    setActiveFilter(evt.target);
+    debouncedRenderPosts(filterDiscussed(originalPosts));
+  });
+
+initiateBigPicture(() => currentPosts);
+
+export { onPostsLoaded };
